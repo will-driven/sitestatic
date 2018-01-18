e991d3f2adfd663bb7ba393038df82f7660b1564
will-driven <mesfinbsns@gmail.com>
2018-01-17 23:09:09 -0500

    add test.js files

---
+++ assets/rideshareandbeyond/js/test2.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+    var itemlength = $('#myCarousel .item').length;
+    var triggerheight = Math.round(boxheight/itemlength+1);
+	$('#myCarousel .list-group-item').outerHeight(triggerheight);
+});
