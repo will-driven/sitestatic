3d8e1087b6edc01dfc5c20c05eb8e9b53fffe82b
will-driven <mesfinbsns@gmail.com>
2018-02-06 01:18:36 -0500

    update js : RSB top nav sticky on scrolll

--- _assets/rideshareandbeyond/js/core.js
+++ _assets/rideshareandbeyond/js/core.js
@@ -51,10 +51,10 @@ $(document).ready(function(){
   $(window).bind('scroll', function() {
   var navHeight = $( window ).height() - 70;
     if ($(window).scrollTop() > navHeight) {
-      $('top-nav').addClass('fixed');
+      $('.top-nav').addClass('top-nav-fixed');
     }
     else {
-      $('top-nav').removeClass('fixed');
+      $('.top-nav').removeClass('top-nav-fixed');
     }
  });
 });
